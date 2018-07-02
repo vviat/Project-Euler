e387ada268fa72d3e5cb83cbb8468b34c8ad8acc
ygy <40139861+vviat@users.noreply.github.com>
2018-07-02 12:04:51 +0800

    亲和数解法二

---
+++ 21.1.c
@@ -0,0 +1,47 @@
+/*************************************************************************
+	> File Name: 21.1.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年05月09日 星期三 12时51分43秒
+ ************************************************************************/
+
+#include<stdio.h>
+#define MAX 10000
+int prime[MAX + 5] = {0};
+int dnum[MAX + 5] = {0};
+int xnum[MAX + 5] = {0};
+void init() {
+    for(int i = 2; i <= MAX; i++) {
+        if(!prime[i]) {
+            prime[++prime[0]] = i;
+            dnum[i] = i + 1;
+            xnum[i] = i * i - 1;
+        }
+        for(int j =1; j<=prime[0]; j++) {
+        if(i * prime[j] > MAX) break; 
+            prime[i * prime[j]] = 1; 
+            if(i % prime[j] == 0) { 
+                xnum[i * prime[j]] = xnum[i] * prime[j] + prime[j] - 1; 
+                dnum[i * prime[j]] = dnum[i] * xnum[i * prime[j]] / xnum[i];
+                break;
+            } else { 
+                dnum[i * prime[j]] = dnum[i] * dnum[prime[j]];
+                xnum[i * prime[j]] = xnum[prime[j]];
+            } 
+        }
+    }
+} 
+int main(){
+    init();
+    for(int i = 2; i <= MAX; ++i) { 
+        dnum[i] -= i; 
+    } 
+    int res = 0; 
+    for(int i = 2; i <= MAX; ++i) { 
+        if(dnum[i] == i) continue; 
+        if(dnum[i] > MAX) continue; 
+        if(dnum[dnum[i]] == i) res += i;  
+    } 
+    printf("%d\n", res);
+    return 0;  
+}
